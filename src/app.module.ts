6fcd109bbe8b86386d2cf7b5998ff23579aa0fa1
Abdelqader <saabood519@gmail.com>
2026-01-14 14:15:12 +0200

    init database

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+import { DatabaseModule } from './modules/database/database.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+
+@Module({
+  imports: [DatabaseModule, AuthModule, UserModule],
+})
+export class AppModule {}
